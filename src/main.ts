7d287c1859f7c8f15934a9e6dc4295484c7c761c
Jamie Gaehring <gaehj457@gmail.com>
2023-06-10 16:55:42 -0400

    Remove Tauri commands from main.ts.
    Learn more about Tauri commands at https://tauri.app/v1/guides/features/command

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,5 @@
-import { invoke } from "@tauri-apps/api/tauri";
 import { createApp } from 'vue';
 // import './style.css';
 import App from './App.vue';
 
 createApp(App).mount('#app')
-
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
-
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document
-    .querySelector("#greet-button")
-    ?.addEventListener("click", () => greet());
-});
