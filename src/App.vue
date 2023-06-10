2e95bee404693bd9b5bb551e613ce2de0456d8cf
Jamie Gaehring <gaehj457@gmail.com>
2023-06-10 16:50:38 -0400

    Reimplement farm list w/ Vue components.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from 'vue';
+import FarmList from './components/FarmList.vue';
+
+const RED = '🔴';
+const YELLOW = '🟡';
+const GREEN = '🟢';
+
+const oneDay = 1000 * 60 * 60 * 24;
+const twoDays = oneDay * 2;
+const threeDays = oneDay * 3;
+
+const farms = reactive([
+  {
+    name: 'Joe\'s Farm',
+    status: RED,
+    timestamp: Date.now() - oneDay,
+  },
+  {
+    name: 'Sally\'s Farm',
+    status: YELLOW,
+    timestamp: Date.now() - twoDays,
+  },
+  {
+    name: 'Sam\'s Farm',
+    status: GREEN,
+    timestamp: Date.now() - threeDays,
+  },
+]);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Richland Gro-Op Early Warning System</h1>
+  <farm-list :farms="farms"></farm-list>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/FarmList.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+defineProps<{ farms: Array<{ name: string, status: string, timestamp: number }> }>()
+</script>
+
+<template>
+  <ul class="farm-list">
+  <li v-for="(farm, i) in farms" :key="`farm-${i}`" class="farm-total">
+    <div class="toggle-drop-down">
+      > 
+    </div>
+    <div class="farm-name">
+      {{ farm.name }}
+    </div>
+    <div class="farm-total-status">
+      {{ farm.status }}
+    </div>
+    <div class="last-checkin">
+      {{ new Date(farm.timestamp).toLocaleDateString() }}
+    </div>
+  </li>
+  </ul>
+</template>
+
+<style scoped>
+.farm-list li {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+
+.farm-list li div {
+  flex-grow: 1;
+  max-width: 100%;
+  text-align: start;
+}
+
+.farm-list li .toggle-drop-down,
+.farm-list li .farm-total-status,
+.farm-list li .last-checkin {
+  max-width: fit-content;
+}
+</style>
